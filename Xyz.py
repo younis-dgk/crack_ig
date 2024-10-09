2383d917a4b71192cda89bcd345670f15f4d18d4
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-09 13:00:43 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -2,8 +2,8 @@ import os,re,sys,platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from IG2 import XYZ
-    XYZ()
+    import ig64
 elif bit == '32bit':
-    print(f"\033[1;91m Sorry 32Bit Not Supported .... ");exit() 
+    print(f"\033[1;91m Sorry Baby 32 Bit Not Supported .... ");exit() 
+ 
  
