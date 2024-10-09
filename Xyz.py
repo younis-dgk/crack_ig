31b8f69b24faf69771b27d03ea8d3b0feac53bb4
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-09 13:05:39 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -1,9 +1,23 @@
-import os,re,sys,platform
-os.system('git pull')
+#coding=utf-8
+import os, sys, platform
+ 
+os.system('rm -rf ig64.so')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf ig64.so')
+except:
+    pass
+ 
+ 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import ig64
-elif bit == '32bit':
-    print(f"\033[1;91m Sorry Baby 32 Bit Not Supported .... ");exit() 
+    if not os.path.isfile('ig64.so'):
+        os.system('curl -L https://github.com/younis-dgk/crack_ig/blob/main/ig64.cpython-312.so?raw=true -o ig64.so') 
+        import ig64
+    else:
+        import ig64
  
+elif bit == '32bit':
+    exit()
  
